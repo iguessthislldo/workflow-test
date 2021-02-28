03221058707e7db45cfed831c2bed060b87504d9
Fred Hornsey <fred@hornsey.us>
2021-02-28 16:50:14 -0600

    Annotations Attempt

--- src/program.py
+++ src/program.py
@@ -1,1 +1,19 @@
-print("Hello, world!")
+# Line 1
+# Line 2
+# Line 3
+# Line 4
+
+print('::warning ::Warning no file')
+print('::error ::Error no file')
+
+print('::warning file={}::Warning File'.format(__file__))
+print('::error file={}::Error File'.format(__file__))
+
+print('::warning file={},line=1::Warning Line 1'.format(__file__))
+print('::error file={},line=2::Error Line 2'.format(__file__))
+
+print('::warning file={},line=3,col=2::Warning Line 3'.format(__file__))
+print('::error file={},line=4,col=4::Error Line 4'.format(__file__))
+
+print('::warning ::Warning Multiline Line%0ALine2%0ALine3')
+print('::error ::Error Multiline Line%0ALine2%0ALine3%0ALine4')

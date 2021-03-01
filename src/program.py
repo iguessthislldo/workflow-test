845ad4689a4d4d7a25f0a02d72a8db2071f924f2
Fred Hornsey <fred@hornsey.us>
2021-03-01 13:28:35 -0600

    Try to get job name env var

--- src/program.py
+++ src/program.py
@@ -3,17 +3,20 @@
 # Line 3
 # Line 4
 
-print('::warning ::Warning no file')
-print('::error ::Error no file')
+import os
+print("Hello", os.environ.get('JOB_NAME'))
 
-print('::warning file={}::Warning File'.format(__file__))
-print('::error file={}::Error File'.format(__file__))
+# print('::warning ::Warning no file')
+# print('::error ::Error no file')
 
-print('::warning file={},line=1::Warning Line 1'.format(__file__))
-print('::error file={},line=2::Error Line 2'.format(__file__))
+# print('::warning file={}::Warning File'.format(__file__))
+# print('::error file={}::Error File'.format(__file__))
 
-print('::warning file={},line=3,col=2::Warning Line 3'.format(__file__))
-print('::error file={},line=4,col=4::Error Line 4'.format(__file__))
+# print('::warning file={},line=1::Warning Line 1'.format(__file__))
+# print('::error file={},line=2::Error Line 2'.format(__file__))
 
-print('::warning ::Warning Multiline Line%0ALine2%0ALine3')
-print('::error ::Error Multiline Line%0ALine2%0ALine3%0ALine4')
+# print('::warning file={},line=3,col=2::Warning Line 3'.format(__file__))
+# print('::error file={},line=4,col=4::Error Line 4'.format(__file__))
+
+# print('::warning file={},line=5::Warning Multiline Line%0ALine2%0ALine3'.format(__file__))
+# print('::error file={},line=6::Error Multiline Line%0ALine2%0ALine3%0ALine4'.format(__file__))
